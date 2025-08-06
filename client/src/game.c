8475e57847cd702dcdd302f93153b991383ae402
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-05 20:02:23 -0400

    Minor refactor 2

--- client/src/game.c
+++ client/src/game.c
@@ -1,4 +1,4 @@
-Transform *camera;
+Transform camera;
 
 Model *model_test;
 
@@ -21,11 +21,10 @@ void on_start() {
 	glClearColor(0.2f, 0.2f, 0.23f, 1.0f);
 	SDL_SetRelativeMouseMode(SDL_TRUE);
 
-	camera = calloc(sizeof(Transform), 1);
+	camera.y = 2;
 
 	// create a model for testing
 	model_test = create_model(miku_mesh, miku_mesh_bytecount, miku_mesh_vertcount, dirt_texture, 16, 16);
-	model_test->transform.z = -2.0;
 
 	// create a chunk for testing
 	for (int x = 0; x < 16; x++)
@@ -35,58 +34,56 @@ void on_start() {
 
 	remesh_chunk(&chunk);
 
-	chunk.model.transform.z = -2.0;
-	chunk.model.transform.y = 2.3;
+	chunk.model.transform.y = -16;
 }
 
 void on_terminate() {
 
-	free(camera);
 	free(model_test);
 }
 
 void process_tick() {
 
 	if (left) {
-		camera->z -= sin(camera->yaw) * 0.1;
-		camera->x -= cos(camera->yaw) * 0.1;
+		camera.z -= sin(camera.yaw) * 0.1;
+		camera.x -= cos(camera.yaw) * 0.1;
 	} else if (right) {
-		camera->z += sin(camera->yaw) * 0.1;
-		camera->x += cos(camera->yaw) * 0.1;
+		camera.z += sin(camera.yaw) * 0.1;
+		camera.x += cos(camera.yaw) * 0.1;
 	}
 
 	if (forward) {
-		camera->z -= cos(camera->yaw) * 0.1;
-		camera->x += sin(camera->yaw) * 0.1;
+		camera.z -= cos(camera.yaw) * 0.1;
+		camera.x += sin(camera.yaw) * 0.1;
 	} else if (backward) {
-		camera->z += cos(camera->yaw) * 0.1;
-		camera->x -= sin(camera->yaw) * 0.1;
+		camera.z += cos(camera.yaw) * 0.1;
+		camera.x -= sin(camera.yaw) * 0.1;
 	}
 
 	if (up) {
-		camera->y += 0.1;
+		camera.y += 0.1;
 	} else if (down) {
-		camera->y -= 0.1;
+		camera.y -= 0.1;
 	}
 
 	model_test->transform.yaw += 0.01;
 
-	draw_model(camera, model_test);
-	draw_model(camera, &chunk.model);
+	draw_model(&camera, model_test);
+	draw_model(&camera, &chunk.model);
 }
 
 void process_event(SDL_Event event) {
 
 	if (event.type == SDL_MOUSEMOTION) {
 
-		camera->pitch += event.motion.yrel * 0.01;
-		camera->yaw += event.motion.xrel * 0.01;
+		camera.pitch += event.motion.yrel * 0.01;
+		camera.yaw += event.motion.xrel * 0.01;
 
 		// clamp camera pitch
-		if (camera->pitch > M_PI / 2) {
-			camera->pitch = M_PI / 2;
-		} else if (camera->pitch < -M_PI / 2) {
-			camera->pitch = -M_PI / 2;
+		if (camera.pitch > M_PI / 2) {
+			camera.pitch = M_PI / 2;
+		} else if (camera.pitch < -M_PI / 2) {
+			camera.pitch = -M_PI / 2;
 		}
 	}
 
